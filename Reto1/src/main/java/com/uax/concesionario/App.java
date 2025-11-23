6093507dcc17827a5bdb892742d9940d53a4faed
Álvaro Martínez Fernández <amarfer26@gmail.com>
2025-11-24 00:33:47 +0100

    23N

--- Reto1/src/main/java/com/uax/concesionario/App.java
+++ Reto1/src/main/java/com/uax/concesionario/App.java
@@ -2,10 +2,9 @@ package com.uax.concesionario;
 
 import com.uax.concesionario.controller.ConcesionarioController;
 
-
-//clase de entrada (entry point) del programa
+//clase de entrada (entry point) del program
 public class App {
-  
+
     public static void main(String[] args) {
         // Instancia controller que gestiona la interfaz de usuario
         ConcesionarioController controller = new ConcesionarioController();
